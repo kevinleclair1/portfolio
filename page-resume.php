f7b997657be503a6118a0ce6f69b990336d0603c
kevinleclair1 <kevinleclair1@gmail.com>
2015-03-15 13:34:32 -0400

    second deploy with resume

--- page-resume.php
+++ page-resume.php
@@ -17,7 +17,18 @@ get_header();  ?>
 			    <?php bloginfo( 'name' ); ?>
 			  </a>
 			</h2>
-			<h3><a href="mailto:hello@kevinleclair.ca">hello@kevinleclair.ca</a> // <a href="kevinleclair.ca">kevinleclair.ca</a> // 905-259-2059</h3>
+		    <?php //we are going to pull in our latest four blog posts ?>
+		    <?php $latestPosts = new WP_Query(array(
+		    	'post_type' => 'resumecontact', //we only want blog posts
+		    	'posts_per_page' => -1
+		    )); ?>
+
+
+			<?php if($latestPosts->have_posts()) while($latestPosts ->have_posts()) : $latestPosts->the_post(); ?>
+				<h3><a href="mailto:<?php the_field('resume_email') ?>"><?php the_field('resume_email') ?></a> // <a href="http://<?php the_field('resume_website') ?>"><?php the_field('resume_website') ?></a> // <?php the_field('resume_phone') ?></h3>
+			<?php endwhile; //end custom loop?>
+			<?php wp_reset_postdata(); // return env back to regular?>
+			
     	</div>
     	<div class="skillsColumn">
           <?php $latestPosts = new WP_Query(array(
@@ -27,12 +38,15 @@ get_header();  ?>
 
 
       	<?php if($latestPosts->have_posts()) while($latestPosts ->have_posts()) : $latestPosts->the_post(); ?>
+      		<div class="skillList">
       		
 	      		<h4><?php the_title(); ?></h4>
 	      		  <?php while( has_sub_field('skills') ): ?>
 	      		    <p><?php echo get_sub_field('skills') ?></p> 
 	      		  <?php endwhile; ?>
+	      	</div>
 	      	<?php endwhile; //end custom loop?>
+
 	      	<?php wp_reset_postdata(); // return env back to regular?>
       	</div>
 		
